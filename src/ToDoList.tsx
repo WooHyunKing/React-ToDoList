145b925e1cb34e2c11f00d555d42ba7c1ab1da12
신 우현 <woo_hyun_1@naver.com>
2023-01-12 00:25:11 +0900

    Learn about react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,115 +1,130 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// const ToDoList = () => {
-//   const [toDo, setToDo] = useState("");
-//   const [toDoError, setToDoError] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDoError("");
-//     setToDo(value);
-//   };
-
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (toDo.length < 10) {
-//       return setToDoError("ToDo shoud be longer");
-//     }
-//     console.log(toDo);
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a Todo" />
-//         <button>Add</button>
-//         {toDoError !== "" ? toDoError : null}
-//       </form>
-//     </div>
-//   );
-// };
-
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
+  toDo: string;
 }
 
 const ToDoList = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-
-  const onValid = (data: any) => {
-    console.log(data);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
-
-  //   console.log(watch());
-  //   watch를 사용함으로써 form의 입력값을 추적할 수 있다.
-
-  console.log(errors);
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        {/* toDo라는 상태를 만들어주고, onChange와 값을 prop으로 전달한다. */}
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register("email", {
-            required: "Email is Required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
+          {...register("toDo", {
+            required: "Please write a ToDo",
           })}
-          placeholder="Write a Email"
+          placeholder="Write a Todo"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", { required: "write here" })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "write here" })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", { required: "write here", minLength: 10 })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", { required: "write here", minLength: 5 })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "Password is required",
-          })}
-          placeholder="Password Check"
-        />
-        <span>{errors?.password1?.message}</span>
-
         <button>Add</button>
       </form>
     </div>
   );
 };
+
+// interface IForm {
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+//   username: string;
+//   password: string;
+//   password1: string;
+//   extraError?: string;
+// }
+
+// const ToDoList = () => {
+//   const {
+//     register,
+//     watch,
+//     handleSubmit,
+//     formState: { errors },
+//     setError,
+//   } = useForm<IForm>({
+//     defaultValues: {
+//       email: "@naver.com",
+//     },
+//   });
+
+//   const onValid = (data: IForm) => {
+//     if (data.password !== data.password1) {
+//       setError(
+//         "password1",
+//         { message: "Password are not the same" },
+//         { shouldFocus: true }
+//       );
+//     }
+//     // extraError는 특정 항목에 해당되는 에러가 아니라, 전체 form에 해당하는 에러
+//     setError("extraError", { message: "Server offline" });
+//   };
+
+//   //   console.log(watch());
+//   //   watch를 사용함으로써 form의 입력값을 추적할 수 있다.
+
+//   console.log(errors);
+//   return (
+//     <div>
+//       <form
+//         style={{ display: "flex", flexDirection: "column" }}
+//         onSubmit={handleSubmit(onValid)}
+//       >
+//         {/* toDo라는 상태를 만들어주고, onChange와 값을 prop으로 전달한다. */}
+//         <input
+//           {...register("email", {
+//             required: "Email is Required",
+//             pattern: {
+//               value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+//               message: "Only naver.com emails allowed",
+//             },
+//           })}
+//           placeholder="Write a Email"
+//         />
+//         <span>{errors?.email?.message}</span>
+//         <input
+//           {...register("firstName", {
+//             required: "write here",
+//             validate: {
+//               noNico: (value) =>
+//                 value.includes("nico") ? "No nico allowed" : true,
+//               noNick: (value) =>
+//                 value.includes("nick") ? "No nick allowed" : true,
+//             },
+//           })}
+//           placeholder="First Name"
+//         />
+//         <span>{errors?.firstName?.message}</span>
+//         <input
+//           {...register("lastName", { required: "write here" })}
+//           placeholder="Last Name"
+//         />
+//         <span>{errors?.lastName?.message}</span>
+//         <input
+//           {...register("username", { required: "write here", minLength: 10 })}
+//           placeholder="Username"
+//         />
+//         <span>{errors?.username?.message}</span>
+//         <input
+//           {...register("password", { required: "write here", minLength: 5 })}
+//           placeholder="Password"
+//         />
+//         <span>{errors?.password?.message}</span>
+//         <input
+//           {...register("password1", {
+//             required: "Password is required",
+//           })}
+//           placeholder="Password Check"
+//         />
+//         <span>{errors?.password1?.message}</span>
+
+//         <button>Add</button>
+
+//         <span>{errors?.extraError?.message}</span>
+//       </form>
+//     </div>
+//   );
+// };
 
 export default ToDoList;
