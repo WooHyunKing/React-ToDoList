26ff34bcdda31c03ebb2c7de2a9da53f5c891bf6
신 우현 <woo_hyun_1@naver.com>
2023-02-22 23:14:15 +0900

    6.11 Add To Do

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -7,9 +7,20 @@ import {
   useSetRecoilState,
 } from "recoil";
 
-interface ITodo {
+interface IForm {
   toDo: string;
 }
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TODO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
 
 const ToDoList = () => {
   const {
@@ -17,15 +28,23 @@ const ToDoList = () => {
     formState: { errors },
     handleSubmit,
     setValue,
-  } = useForm<ITodo>();
+  } = useForm<IForm>();
 
-  const onValid = (data: ITodo) => {
+  const [toDo, setToDo] = useRecoilState<IToDo[]>(toDoState);
+
+  const onValid = (data: IForm) => {
     console.log(data?.toDo);
+    setToDo((item) => [
+      { text: data.toDo, id: Date.now(), category: "TODO" },
+      ...item,
+    ]);
     setValue("toDo", "");
   };
 
   return (
     <div>
+      <h1>My ToDoList</h1>
+      <hr></hr>
       <form onSubmit={handleSubmit(onValid)}>
         <input
           {...register("toDo", {
@@ -36,157 +55,13 @@ const ToDoList = () => {
         <button>Add</button>
         <span>{errors?.toDo?.message}</span>
       </form>
+      <ul>
+        {toDo.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
-
-// interface IForm {
-//   toDo: string;
-// }
-
-// interface IToDo {
-//   text: string;
-//   id: number;
-//   category: "TO_DO" | "DOING" | "DONE";
-// }
-
-// const toDoState = atom<IToDo[]>({
-//   key: "toDo",
-//   default: [],
-// });
-
-// const ToDoList = () => {
-//   const [toDos, setToDos] = useRecoilState(toDoState);
-//   const { register, handleSubmit, setValue } = useForm<IForm>();
-//   const handleValid = ({ toDo }: IForm) => {
-//     setToDos((oldToDos) => [
-//       { text: toDo, id: Date.now(), category: "TO_DO" },
-//       ...oldToDos,
-//     ]);
-//     setValue("toDo", "");
-//   };
-//   console.log(toDos);
-//   return (
-//     <div>
-//       <h1>Todo List</h1>
-//       <hr />
-//       <form onSubmit={handleSubmit(handleValid)}>
-//         <input
-//           {...register("toDo", {
-//             required: "Please write a ToDo",
-//           })}
-//           placeholder="Write a Todo"
-//         />
-//         <button>Add</button>
-//       </form>
-//       <ul>
-//         {toDos.map((toDo) => (
-//           <li key={toDo.id}>{toDo.text}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// interface IForm {
-//   email: string;
-//   firstName: string;
-//   lastName: string;
-//   username: string;
-//   password: string;
-//   password1: string;
-//   extraError?: string;
-// }
-
-// const ToDoList = () => {
-//   const {
-//     register,
-//     watch,
-//     handleSubmit,
-//     formState: { errors },
-//     setError,
-//   } = useForm<IForm>({
-//     defaultValues: {
-//       email: "@naver.com",
-//     },
-//   });
-
-//   const onValid = (data: IForm) => {
-//     if (data.password !== data.password1) {
-//       setError(
-//         "password1",
-//         { message: "Password are not the same" },
-//         { shouldFocus: true }
-//       );
-//     }
-//     // extraError는 특정 항목에 해당되는 에러가 아니라, 전체 form에 해당하는 에러
-//     setError("extraError", { message: "Server offline" });
-//   };
-
-//   //   console.log(watch());
-//   //   watch를 사용함으로써 form의 입력값을 추적할 수 있다.
-
-//   console.log(errors);
-//   return (
-//     <div>
-//       <form
-//         style={{ display: "flex", flexDirection: "column" }}
-//         onSubmit={handleSubmit(onValid)}
-//       >
-//         {/* toDo라는 상태를 만들어주고, onChange와 값을 prop으로 전달한다. */}
-//         <input
-//           {...register("email", {
-//             required: "Email is Required",
-//             pattern: {
-//               value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-//               message: "Only naver.com emails allowed",
-//             },
-//           })}
-//           placeholder="Write a Email"
-//         />
-//         <span>{errors?.email?.message}</span>
-//         <input
-//           {...register("firstName", {
-//             required: "write here",
-//             validate: {
-//               noNico: (value) =>
-//                 value.includes("nico") ? "No nico allowed" : true,
-//               noNick: (value) =>
-//                 value.includes("nick") ? "No nick allowed" : true,
-//             },
-//           })}
-//           placeholder="First Name"
-//         />
-//         <span>{errors?.firstName?.message}</span>
-//         <input
-//           {...register("lastName", { required: "write here" })}
-//           placeholder="Last Name"
-//         />
-//         <span>{errors?.lastName?.message}</span>
-//         <input
-//           {...register("username", { required: "write here", minLength: 10 })}
-//           placeholder="Username"
-//         />
-//         <span>{errors?.username?.message}</span>
-//         <input
-//           {...register("password", { required: "write here", minLength: 5 })}
-//           placeholder="Password"
-//         />
-//         <span>{errors?.password?.message}</span>
-//         <input
-//           {...register("password1", {
-//             required: "Password is required",
-//           })}
-//           placeholder="Password Check"
-//         />
-//         <span>{errors?.password1?.message}</span>
-
-//         <button>Add</button>
-
-//         <span>{errors?.extraError?.message}</span>
-//       </form>
-//     </div>
-//   );
-// };
 
 export default ToDoList;
