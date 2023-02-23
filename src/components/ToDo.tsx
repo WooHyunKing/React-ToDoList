aa3a362ea651d997509331676c042f471aefd683
신 우현 <woo_hyun_1@naver.com>
2023-02-23 20:00:27 +0900

    Create categories and set Todos by js

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useSetRecoilState } from "recoil";
 import { IToDo } from "../interfaces/form";
+import { toDoState } from "../Recoil/atom";
 
-const ToDo = ({ text }: IToDo) => {
+const ToDo = ({ text, id, category }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (propCategory: IToDo["category"]) => {
+    console.log("I want to change ", propCategory);
+    setToDos((prevToDos) => {
+      const targetIndex = prevToDos.findIndex((todo) => todo.id === id);
+      const newToDo = { text, id, category: propCategory };
+
+      return [
+        ...prevToDos.slice(0, targetIndex),
+        newToDo,
+        ...prevToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TODO" && (
+        <button onClick={() => onClick("TODO")}>TODO</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>DONE</button>
+      )}
     </li>
   );
 };
